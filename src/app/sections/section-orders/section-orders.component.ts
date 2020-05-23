5f67d360b7ec3f93a0a03e959f822ffcf9dafa82
admin <annanihao@yandex.ru>
2020-05-23 18:37:06 +0800

    Start pagination component

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../../shared/order';
+import { SalesDataService } from '../../services/sales-data.service';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,28 +9,48 @@ import { Order } from '../../shared/order';
 })
 export class SectionOrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _salesData: SalesDataService) { }
 
-  orders: Order[] = [
-    {id: 1,customer: 
-        {id:1 ,name:'La Bakery',state:'Co',email:'huhu@gmail.com'},
-        total:230,placed:new Date(2018,12,3),fulfilled:new Date(2018,12,5)},
-    {id: 2,customer: 
-        {id:34 ,name:'Pa Bakery',state:'Co',email:'huhu@gmail.com'},
-        total:230,placed:new Date(2018,12,3),fulfilled:new Date(2018,12,5)},
-    {id: 3,customer: 
-       {id:2 ,name:'La Ship',state:'Co',email:'huhu@gmail.com'},
-       total:230,placed:new Date(2018,12,3),fulfilled:new Date(2018,12,5)},
-    {id: 4,customer: 
-      {id:11 ,name:'La Constustion',state:'Co',email:'huhu@gmail.com'},
-      total:230,placed:new Date(2018,12,3),fulfilled:new Date(2018,12,5)},
-    {id: 5,customer: 
-        {id:19 ,name:'Lape Bakery',state:'Co',email:'huhu@gmail.com'},
-         total:230,placed:new Date(2018,12,3),fulfilled:new Date(2018,12,5)},
-  ];
-
+  orders: Order[];
+  total = 0;
+  page = 1;
+  limit = 10;
+  loading = false;
 
   ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders(): void{
+    this._salesData.getOrders(this.page,this.limit)
+      .subscribe(res => {
+        this.orders = res['page']['data'];
+        this.total  = res['page'].total;
+        this.loading = false;
+         
+      });
+  }
+
+  goToPrevious(): void{
+    //console.log('Previous bottom clicked');
+    
+    //to decrease value by one:
+    this.page--;
+    this.getOrders();
+  }
+
+  goToNext(): void{
+    //console.log('Next bottom clicked');
+
+    this.page++;
+    this.getOrders();
+  }
+
+
+  goToPage(n:number): void{
+    this.page = n;
+    this.getOrders();
   }
 
 }
+
---
+++ src/app/services/sales-data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SalesDataService{
+
+    constructor(private _http: Http) { }
+
+    getOrders(pageIndex: number, pageSize: number) {
+        return this._http.get('http://localhost:41794/api/order/' + pageIndex + '/' + pageSize)
+          .map(res => res.json());
+      }
+    
+    getOrdersByCustomer(n: number) {
+        return this._http.get('http://localhost:41794/api/order/bycustomer/' + n)
+          .map(res => res.json());
+      }
+    
+    getOrdersByState() {
+        return this._http.get('http://localhost:41794/api/order/bystate/')
+          .map(res => res.json());
+      }
+
+}
