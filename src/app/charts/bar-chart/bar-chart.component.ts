f709b18c6ed6fd18e7f4a65ab6578a5bbfd2c3ec
admin <annanihao@yandex.ru>
2020-05-29 13:45:07 +0800

    Implement pagination and bar-chart component

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesDataService } from '../../services/sales-data.service';
+//for working with dates:
+import * as moment from 'moment';
 
-const SAMPLE_BARCHART_DATA: any[]=[
-  {data:[38,24,78,12,34,87,55], label:'Q3 Sales'},
-  {data:[58,23,90,12,84,21,95], label:'Q4 Sales'},
-];
+// const SAMPLE_BARCHART_DATA: any[]=[
+//   {data:[38,24,78,12,34,87,55], label:'Q3 Sales'},
+//   {data:[58,23,90,12,84,21,95], label:'Q4 Sales'},
+// ];
 
-const SAMPLE_BARCHART_LABELS: string[]=['w1','w2','w3','w4','w5','w6','w7'];
-
-
+// const SAMPLE_BARCHART_LABELS: string[]=['w1','w2','w3','w4','w5','w6','w7'];
 
 @Component({
   selector: 'app-bar-chart',
@@ -16,10 +17,14 @@ const SAMPLE_BARCHART_LABELS: string[]=['w1','w2','w3','w4','w5','w6','w7'];
 })
 export class BarChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _salesDataService: SalesDataService) { }
 
-  public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  orders: any;
+  orderLabels:string[];
+  orderData:number[];
+
+  public barChartData: any[];
+  public barChartLabels: string[];
   public barChartLegend =true;
   public barChartType = 'bar';
   public barChartOptions: any = {
@@ -29,6 +34,40 @@ export class BarChartComponent implements OnInit {
 
 
   ngOnInit(): void {
+    //the most recent 100 orders:
+    this._salesDataService.getOrders(1,100)
+      .subscribe(res =>{
+        //console.log(res['page']['data']);
+        const localChartData = this.getChartData(res);
+        this.barChartLabels = localChartData.map(x => x[0]).reverse();
+        this.barChartData = [{'data':localChartData.map(x => x[1]),'label':'Sales'}];
+      });
   }
 
+  getChartData(res: Response){
+    this.orders = res['page']['data'];
+    const data = this.orders.map(o => o.total);
+    //just to see how it looks like:
+    // console.log(data);
+    //const labels = this.orders.map(o => moment(new Date (o.placed)).format('YY-MM-DD'));
+    //console.log(labels);
+    const formattedOrders = this.orders.reduce((r,e) => {
+      r.push([moment(e.placed).format('YY-MM-DD'),e.total]);
+      return r;
+    },[]);
+
+    const p = [];
+
+    const charData = formattedOrders.reduce((r,e) => {
+      const key = e[0];
+      if(!p[key]){
+        p[key]= e;
+        r.push(p[key]);
+      } else{
+        p[key][1] += e[1];
+      }
+      return r;
+    },[]);
+    return charData;
+  }
 }
--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -42,4 +42,42 @@ export class PaginationComponent implements OnInit {
     return this.page * this.perPage >= this.count;
   }
 
+  getMin(): number {
+    return ((this.perPage * this.page) - this.perPage) + 1;
+  }
+
+  getMax(): number {
+    let max = this.perPage * this.page;
+    if (max > this.count) {
+      max = this.count;
+    }
+    return max;
+  }
+
+  getPages(): number[] {
+    const totalPages = Math.ceil(this.count/this.perPage);
+    const thisPage = this.page || 1;
+    const pagesToShow = this.pagesToShow || 9;
+    const pages: number[] = [];
+    pages.push(thisPage);
+
+    console.log('Starting loop with: total pages:', totalPages, 'thisPage:', thisPage, 'pagesToShow:', pagesToShow );
+    for (let i = 0 ; i < pagesToShow - 1; i++){
+      console.log('pages[]:', pages);
+      if (pages.length < pagesToShow) {
+        if (Math.min.apply(null, pages) > 1) {
+          pages.push(Math.min.apply(null, pages) - 1);
+          console.log('pushing', Math.min.apply(null, pages) - 1, 'onto array');
+        }
+      }
+      if (pages.length < pagesToShow) {
+        if (Math.max.apply(null, pages) < totalPages) {
+          pages.push(Math.max.apply(null, pages) + 1);
+          console.log('pushing', Math.max.apply(null, pages) + 1, 'onto array');
+        }
+      }
+    }
+    pages.sort((a, b) => a - b);
+    return pages;
+  }
 }
