e8e4918c164edf4ee3a56fcdf0c74b057ab318ad
admin <annanihao@yandex.ru>
2020-05-29 19:30:46 +0800

    Add themes and pie chart functionality

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import _ from 'lodash';
+import {THEME_COLORS} from '../../shared/theme.colors';
+
+const theme = 'Default';
 
 @Component({
   selector: 'app-pie-chart',
@@ -9,18 +13,37 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
-  pieChartData: number[] = [350,456,190];
-  pieChartLabels: string[] = ['DHL','JJJ','CTR'];
+  @Input() inputData: any;
+  @Input() limit: number;
+
+  pieChartData: number[];
+  pieChartLabels: string[];
   pieChartType = 'doughnut';
   pieChartColors: any[] = [
     {
-      backgroundColor:['#26547c','#ff6b6b','#ffd166'],
+      backgroundColor:this.themeColors(theme),
       borderColor: '#111',
     }
   ];
   pieChartLegend = true;
 
   ngOnInit(): void {
+    this.parseChartData(this.inputData,this.limit);
+  }
+
+  parseChartData(res: any, limit?: number){
+    const allData = res.slice(0,limit);
+
+    this.pieChartData = allData.map(x=> _.values(x)[1]);
+    this.pieChartLabels = allData.map(x=> _.values(x)[0]);
+
+  }
+
+  themeColors(setName:string):string[] {
+    const c = THEME_COLORS.slice(0)
+    .find(set=>set.name === setName).colorSet;
+
+    return c;
   }
 
 }
